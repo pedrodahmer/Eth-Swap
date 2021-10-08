9cfd8c6c456ca0f8867d59248385ed33f82a45be
Pedro Dahmer <pedrohd1213@gmail.com>
2021-10-08 11:53:20 -0300

    tests work

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -1,35 +1,41 @@
 const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
-//Configuring chai
+//Importing Chai
 var chai = require("chai");
 var assert = chai.assert; // Using Assert style
-var expect = chai.expect; // Using Expect style
-var should = chai.should(); // Using Should style
 
 contract("EthSwap", (accounts) => {
+  let token, ethSwap;
+
+  //A helper that converts the token number to Wei
+  function tokens(n) {
+    return new web3.utils.toWei(n, 'ether');
+  }
+
+  before(async () => {
+    token = await Token.new();
+    ethSwap = await EthSwap.new();
+    await token.transfer(ethSwap.address, "1000000000000000000000000");
+  })
+  
   //Token was deployed correctly
   describe("Token deployment", async () => {
     it("contract has a name", async () => {
-      let token = await Token.new();
-      const name = await Token.name();
-      assert.equal(name, "DApp Token ");
+      const name = await token.name();
+      assert.equal(name, "DApp Token");
     });
   });
 
   describe("EthSwap deployment", async () => {
     //Token was deployed correctly
     it("contract has a name", async () => {
-      let ethSwap = await EthSwap.new();
       const name = await ethSwap.name();
       assert.equal(name, "EthSwap Instant Exchange ");
     });
 
     //EthSwap has all the tokens
     it("contract has tokens", async () => {
-      let token = await Token.new();
-      let ethSwap = await EthSwap.new();
-      await token.transfer(ethSwap.address, "1000000000000000000000000");
       let balance = await token.balanceOf(ethSwap.address);
       assert.equal(balance.toString(), "1000000000000000000000000");
     });
